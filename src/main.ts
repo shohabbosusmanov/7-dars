490821d4e7e65d79d0c67a087ea740a629ba6d1e
Shohabbos Usmanov <shohabbosusmanov@gmail.com>
2025-06-01 15:51:55 +0500

    scdc

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
-  console.log('Server is running on port', 3000);
 }
 bootstrap();
